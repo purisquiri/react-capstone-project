7447c02081cf9e17e9081eeab6fc5e2f4c9c347b
purisquiri <mau.dorigo@outlook.es>
2021-05-18 10:07:32 -0700

    PropTypes installed and validating the Image component object

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,30 @@
 import React, { useState, useContext } from "react";
+import PropTypes from "prop-types";
 import { Context } from "../Context";
 
 export default function Image({ className, img }) {
   const [isHovered, setIsHovered] = useState(false);
   const { toggleFavorite } = useContext(Context);
+
+  function heartIcon() {
+    if (img.isFavorite) {
+      return (
+        <i
+          className="ri-heart-fill favorite"
+          onClick={() => toggleFavorite(img.id)}
+        ></i>
+      );
+    } else if (isHovered) {
+      return (
+        <i
+          className="ri-heart-line favorite"
+          onClick={() => toggleFavorite(img.id)}
+        ></i>
+      );
+    }
+  }
+
+  const cartIcon = isHovered && <i className="ri-add-circle-line cart"></i>;
 
   return (
     <div
@@ -12,13 +33,18 @@ export default function Image({ className, img }) {
       onMouseLeave={() => setIsHovered(false)}
     >
       <img src={img.url} className="image-grid" alt="random photos" />
-      {isHovered && (
-        <i
-          className="ri-heart-line favorite"
-          onClick={() => toggleFavorite(img.id)}
-        ></i>
-      )}
-      {isHovered && <i className="ri-add-circle-line cart"></i>}
+
+      {heartIcon()}
+      {cartIcon}
     </div>
   );
 }
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool,
+  }),
+};
