61e58eec7bb44e91c25c10d3a763d2d67603046f
purisquiri <mau.dorigo@outlook.es>
2021-05-17 17:09:10 -0700

    fetching photos and passing through context

--- src/Context.js
+++ src/Context.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 const Context = React.createContext();
 
 function ContextProvider({ children }) {
-  return <Context.Provider value="">{children}</Context.Provider>;
+  const [allPhotos, setallPhotos] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch(
+      "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setallPhotos(data));
+  };
+
+  return <Context.Provider value={{ allPhotos }}>{children}</Context.Provider>;
 }
 export { Context, ContextProvider };
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,4 +5,3 @@ export function getClass(i) {
     return "wide";
   }
 }
-utils;
